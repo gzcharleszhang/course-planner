92838d0267092ada24d120f393e5d078ab7083a9
Kevin Hua <kevinhuafamily@gmail.com>
2019-07-29 14:02:47 -0400

    Fixed up script

--- scripts/waterloo-api-connector.py
+++ scripts/waterloo-api-connector.py
@@ -9,28 +9,29 @@ and collects it into JSON files by subject in data/courses
 API_KEY = 'dbcdc71eb794474613d99a69db2bd3aa'
 ALL_COURSES_URL = 'https://api.uwaterloo.ca/v2/courses.json'
 COURSE_ID_URL = 'https://api.uwaterloo.ca/v2/courses/'
+JSON_PATH = '../data/courses/'
 
 # Get all course IDs
-courseIds = []
+course_ids = []
 courses = requests.get(ALL_COURSES_URL, params={'key': API_KEY})
 for course in courses.json()['data']:
-    courseIds.append(course['course_id'])
+    course_ids.append(course['course_id'])
 
 # Get JSON for individual courses and group it with the appropriate subject
 subjects = {}
-for courseId in courseIds:
-    url = COURSE_ID_URL + courseId + '.json'
-    idRequest = requests.get(url, params={'key': API_KEY})
+for course_id in course_ids:
+    url = COURSE_ID_URL + course_id + '.json'
+    id_request = requests.get(url, params={'key': API_KEY})
 
-    subject = idRequest.json()['data']['subject']
+    subject = id_request.json()['data']['subject']
     if subject in subjects:
-        subjects[subject].append(idRequest.json()['data'])
+        subjects[subject].append(id_request.json()['data'])
     else:
-        subjects[subject] = [idRequest.json()['data']]
+        subjects[subject] = [id_request.json()['data']]
 
 # Write JSON for each subject into the appropriate file
 for key in subjects.keys():
-    jsonDict = {'data': subjects[key]}
-    filePath = '../../../data/courses/' + key + '.json'
-    with open(filePath, 'w') as file:
-        json.dump(jsonDict, file)
+    json_dict = {'data': subjects[key]}
+    file_path = JSON_PATH + key + '.json'
+    with open(file_path, 'w') as file:
+        json.dump(json_dict, file)
